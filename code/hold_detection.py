7719c11cf38070082b8c7e68ea94b2f65ff626aa
Jake Wu-Chen <jwcgolf12@gmail.com>
2025-03-23 22:33:15 -0400

    beginning to combine otsu and canny

--- code/hold_detection.py
+++ code/hold_detection.py
@@ -1,6 +1,8 @@
 from canny import canny_thres_image, get_image
 from otsu import otsu_thresh_image
-
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 """
     Image is a file path
@@ -10,12 +12,21 @@ def detect_hold(image):
     otsu_image = get_image(image)
     otsu = otsu_thresh_image(otsu_image)
     
-    print(otsu.shape)
-    print(canny.shape)
     height, width = otsu.shape[:2]
+    image = cv2.imread(image)
     for i_x in range(width): 
         for i_y in range(height): 
             # compare ostu[i_y, i_x] with canny[i_y, i_x]
-
+            # print('otsu', otsu[i_y, i_x])
+            # print('canny', canny[i_y, i_x])
+            if np.array_equal(canny[i_y, i_x], [0, 255, 0]):
+                image[i_y, i_x] = (0, 255, 0)
+            elif np.array_equal(otsu[i_y, i_x], [0, 0, 0]):
+                image[i_y, i_x] = (0, 0, 0)
+    return image
 if __name__ == '__main__':
-    detect_hold('data/V0/1.jpg')
+    pic = detect_hold('data/V0/1.jpg')
+    f, plots = plt.subplots(2, 1)  
+    plots[0].imshow(cv2.imread('data/V0/1.jpg'))
+    plots[1].imshow(pic) 
+    plt.show()
